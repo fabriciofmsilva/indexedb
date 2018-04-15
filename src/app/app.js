d198fe19dc3009920758d85ea2ccfa4b20360cce
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-04-15 14:08:52 -0300

    Add app

---
+++ src/app/app.js
@@ -0,0 +1,6 @@
+import { Person } from './Person.js';
+import { Animal } from './Animal.js';
+
+const person = new Person('Flávio Almeida');
+const animal = new Animal('Calopsita');
+// como realizar a persistência no banco IndexedDB?
